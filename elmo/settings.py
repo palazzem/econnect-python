f3ec44c028414c3ef9e3203a6e6bb9f93cd96fbe
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2019-01-24 00:01:42 +0100

    [core] add simple settings configured via environment variables

--- elmo/settings.py
+++ elmo/settings.py
@@ -1,6 +1,12 @@
 from os import getenv
 
 
-AUTH_ENDPOINT = getenv("ALERTING_ENDPOINT")
+# Environment variables are required to configure the system
+# TODO: implement a Config class that raise exceptions if
+# not properly configured
+BASE_URL = getenv("ALERTING_ENDPOINT")
+VENDOR = getenv("ALERTING_VENDOR")
+
+# Credentials
 USERNAME = getenv("ALERTING_USERNAME")
 PASSWORD = getenv("ALERTING_PASSWORD")
