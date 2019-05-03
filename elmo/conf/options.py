8cbd8dd0966cca7b73aa83aeec928928f8297a02
Emanuele Palazzetti <hello@palazzetti.me>
2019-05-03 19:08:02 +0200

    [config] add validate method that includes a built-in validator to avoid None values

--- elmo/conf/options.py
+++ elmo/conf/options.py
@@ -1,26 +1,11 @@
-from .exceptions import InvalidConfig
-
-
 class Option(object):
     """Settings option that includes built-in validation."""
 
-    def __init__(
-        self,
-        read_only=False,
-        required=False,
-        default=None,
-        allow_null=True,
-        validators=None,
-    ):
+    def __init__(self, default=None, allow_null=True, validators=None):
         """Initialize the option field with permissive defaults.
         By default the field is not required and None value is permitted.
         """
-        # Assert invalid kwargs
-        if required and not default:
-            raise InvalidConfig("Required option cannot default to empty value")
-
-        self.read_only = read_only
-        self.required = required
+        self.value = default if default else None
         self.default = default
         self.allow_null = allow_null
 
@@ -28,3 +13,20 @@ class Option(object):
             self.validators = list(validators)
         else:
             self.validators = []
+
+    def _validate(self):
+        """Validate this Option based on attributes and validators.
+
+        Return:
+            A boolean that is True if the option honors all validators, False otherwise.
+        """
+        failed_validators = []
+        if self.allow_null is False and self.value is None:
+            failed_validators.append("allow_null")
+
+        for validator in self.validators:
+            if validator(self.value) is False:
+                failed_validators.append(validator.__name__)
+
+        is_valid = len(failed_validators) == 0
+        return is_valid, failed_validators
--- tests/test_options.py
+++ tests/test_options.py
@@ -1,36 +1,97 @@
 import pytest
 
-from elmo.conf.exceptions import InvalidConfig
 from elmo.conf.options import Option
 
 
 def test_default_constructor():
     """Should create a permissive config option"""
     option = Option()
-    assert option.read_only is False
-    assert option.required is False
+    assert option.value is None
+    assert option.default is None
     assert option.allow_null is True
     assert option.validators == []
 
 
-def test_invalid_required_default():
-    """Should raise an exception if invalid configuration is used"""
-    with pytest.raises(InvalidConfig):
-        Option(required=True)
-
-
-def test_valid_required_default():
-    """Should accept required kwarg with a default"""
-    option = Option(required=True, default="test")
-    assert option.required is True
-    assert option.default == "test"
+def test_default_change_value():
+    """Setting a default should change the value"""
+    option = Option(default="test")
+    assert option.value == "test"
 
 
 def test_validators_list():
     """Should expect validators to be a list"""
 
-    def fn():
+    def fn(value):
         pass
 
     option = Option(validators=[fn])
     assert option.validators == [fn]
+
+
+def test_validators_not_alist():
+    """Should fail if validators are not a list"""
+
+    def fn(value):
+        pass
+
+    with pytest.raises(TypeError):
+        Option(validators=fn)
+
+
+def test_validate_ok():
+    """Validate should succeed with permissive defaults"""
+    option = Option()
+    assert option._validate() == (True, [])
+
+
+def test_validate_allow_null_ok():
+    """Validate should succeed if the field has None value"""
+    option = Option(allow_null=True)
+    option.value = None
+    assert option._validate() == (True, [])
+
+
+def test_validate_allow_null_fail():
+    """Validate should fail if the field has None value"""
+    option = Option(allow_null=False)
+    option.value = None
+    assert option._validate() == (False, ["allow_null"])
+
+
+def test_validate_with_validators_ok():
+    """Validate should succeed if validators return True"""
+
+    def v1(value):
+        return True
+
+    def v2(value):
+        return True
+
+    option = Option(validators=[v1, v2])
+    assert option._validate() == (True, [])
+
+
+def test_validate_with_all_validators_fail():
+    """Validate should fail if all validators return False"""
+
+    def v1(value):
+        return False
+
+    def v2(value):
+        return False
+
+    option = Option(validators=[v1, v2])
+    assert option._validate() == (False, ["v1", "v2"])
+
+
+def test_validate_with_validators_fail():
+    """Validate should fail if a validator returns False"""
+
+    def v1(value):
+        return True
+
+    def v2(value):
+        return False
+
+    option = Option(validators=[v1, v2])
+    assert option._validate() == (False, ["v2"])
