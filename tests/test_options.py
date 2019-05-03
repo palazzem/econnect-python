647458bd8fc57d752cbaee196349ec7a88e20fd4
Emanuele Palazzetti <hello@palazzetti.me>
2019-05-03 16:59:26 +0200

    [tests] linting

--- tests/test_options.py
+++ tests/test_options.py
@@ -7,22 +7,22 @@ from elmo.conf.options import Option
 def test_default_constructor():
     """Should create a permissive config option"""
     option = Option()
-    assert option.read_only == False
-    assert option.required == False
-    assert option.allow_null == True
+    assert option.read_only is False
+    assert option.required is False
+    assert option.allow_null is True
     assert option.validators == []
 
 
 def test_invalid_required_default():
     """Should raise an exception if invalid configuration is used"""
     with pytest.raises(InvalidConfig):
-        option = Option(required=True)
+        Option(required=True)
 
 
 def test_valid_required_default():
     """Should accept required kwarg with a default"""
     option = Option(required=True, default="test")
-    assert option.required == True
+    assert option.required is True
     assert option.default == "test"
 
 
