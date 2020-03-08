66e97054751507e14e15ec28241062d50d550419
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2020-03-08 18:44:24 +0100

    [core/utils] Remove get_access_token and get_api_url unused code

--- elmo/utils/parser.py
+++ elmo/utils/parser.py
@@ -1,27 +1,4 @@
-from uuid import UUID
 from bs4 import BeautifulSoup
-import re
-
-
-def get_access_token(html):
-    """Retrieve the access token from a HTML page.
-
-    Args:
-        html: the HTML body containing the access token
-    Returns:
-        A string with the access token, None otherwise
-    """
-    start = html.find("var sessionId = '") + 17
-    end = start + 36
-    token = html[start:end]
-
-    try:
-        # Validate if the token is a valid UUID
-        UUID(token, version=4)
-    except ValueError:
-        token = None
-
-    return token
 
 
 def get_listed_items(html):
@@ -37,16 +14,3 @@ def get_listed_items(html):
     tree = BeautifulSoup(html, "html.parser")
     rows = tree.select("tbody > tr")
     return [x.getText().split("\n")[1] for x in rows]
-
-
-def get_api_url(html):
-    """Retrieve the url for API requests from a HTML page.
-
-    Args:
-        html: the HTML body containing the apiURL
-    Returns:
-        A string with the apiURL, None otherwise
-    """
-    apiURL = re.search(r"var apiURL = '(.+)/api/';", html).group(1)
-
-    return apiURL
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,33 +1,5 @@
-from elmo.utils.parser import get_access_token, get_listed_items
+from elmo.utils.parser import get_listed_items
 from elmo.utils.response_helper import slice_list
-
-
-def test_retrieve_access_token():
-    """Should retrieve an access token from the given HTML page."""
-    html = """<script type="text/javascript">
-        var apiURL = 'https://example.com';
-        var sessionId = '00000000-0000-0000-0000-000000000000';
-        var canElevate = '1';
-    """
-    token = get_access_token(html)
-    assert token == "00000000-0000-0000-0000-000000000000"
-
-
-def test_retrieve_access_token_wrong():
-    """Should retrieve an access token from the given HTML page."""
-    html = """<script type="text/javascript">
-        var apiURL = 'https://example.com';
-        var sessionId = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
-        var canElevate = '1';
-    """
-    token = get_access_token(html)
-    assert token is None
-
-
-def test_retrieve_access_token_empty():
-    """Should retrieve an access token from the given HTML page."""
-    token = get_access_token("")
-    assert token is None
 
 
 def test_retrieve_areas_names(areas_html):
