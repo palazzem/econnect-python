8ad5ad2f5d586ef2d90669f29a147f4fdddef7e0
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2023-08-27 13:21:56 +0000

    tests: add ElmoClient fixture to be used in integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,36 @@
 import pytest
 import responses
+from fixtures import responses as r
 
 from elmo.api.client import ElmoClient
 from elmo.devices import AlarmDevice
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def client():
-    """Create an ElmoClient with unlimited expiration time."""
-    client = ElmoClient("https://example.com", "domain")
-    yield client
+    """Creates an instance of `ElmoClient` which emulates the behavior of a real client for
+    testing purposes.
+
+    Although this client instance operates with mocked calls, it is designed to function as
+    if it were genuine. This ensures that the client's usage in tests accurately mirrors how it
+    would be employed in real scenarios.
+
+    Use it for integration tests where a realistic interaction with the `ElmoClient` is required
+    without actual external calls.
+    """
+    client = ElmoClient(base_url="https://example.com", domain="domain")
+    with responses.RequestsMock(assert_all_requests_are_fired=False) as server:
+        server.add(responses.GET, "https://example.com/api/login", body=r.LOGIN, status=200)
+        server.add(responses.POST, "https://example.com/api/updates", body=r.UPDATES, status=200)
+        server.add(responses.POST, "https://example.com/api/panel/syncLogin", body=r.SYNC_LOGIN, status=200)
+        server.add(responses.POST, "https://example.com/api/panel/syncLogout", body=r.SYNC_LOGOUT, status=200)
+        server.add(
+            responses.POST, "https://example.com/api/panel/syncSendCommand", body=r.SYNC_SEND_COMMAND, status=200
+        )
+        server.add(responses.POST, "https://example.com/api/strings", body=r.STRINGS, status=200)
+        server.add(responses.POST, "https://example.com/api/areas", body=r.AREAS, status=200)
+        server.add(responses.POST, "https://example.com/api/inputs", body=r.INPUTS, status=200)
+        yield client
 
 
 @pytest.fixture
